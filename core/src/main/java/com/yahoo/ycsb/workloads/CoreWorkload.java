cac45256db79308966cc67a04011b92d1b37a14d
DragosPetruTaraban <dragos.petru@couchbase.com>
2024-11-22 10:46:02 +0000

    Change prefix again for testing

--- core/src/main/java/com/yahoo/ycsb/workloads/CoreWorkload.java
+++ core/src/main/java/com/yahoo/ycsb/workloads/CoreWorkload.java
@@ -72,7 +72,7 @@ public class CoreWorkload extends Workload {
   /**
    * The default name of the database table to run queries against.
    */
-  public static final String TABLENAME_PROPERTY_DEFAULT = "usertable";
+  public static final String TABLENAME_PROPERTY_DEFAULT = "_sync";
 
   protected String table;
 
