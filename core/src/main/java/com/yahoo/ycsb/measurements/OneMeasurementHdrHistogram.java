62fc2a193b7d1ec38bf0c33c4ddc3c93424f7163
DragosPetruTaraban <dragos.petru@couchbase.com>
2026-04-22 13:38:23 +0100

    Update HdrHistogram reporting

--- core/src/main/java/com/yahoo/ycsb/measurements/OneMeasurementHdrHistogram.java
+++ core/src/main/java/com/yahoo/ycsb/measurements/OneMeasurementHdrHistogram.java
@@ -19,6 +19,7 @@ package com.yahoo.ycsb.measurements;
 
 import com.yahoo.ycsb.measurements.exporter.MeasurementsExporter;
 import org.HdrHistogram.Histogram;
+import org.HdrHistogram.HistogramIterationValue;
 import org.HdrHistogram.HistogramLogWriter;
 import org.HdrHistogram.Recorder;
 
@@ -52,13 +53,24 @@ public class OneMeasurementHdrHistogram extends OneMeasurement {
   /**
    * The default value for the hdrhistogram.percentiles property.
    */
-  public static final String PERCENTILES_PROPERTY_DEFAULT = "95,99";
+  public static final String PERCENTILES_PROPERTY_DEFAULT = "25,50,60,75,85,95,99";
+  
+  /**
+   * The name of the property for determining if we should print out the buckets.
+   */
+  public static final String VERBOSE_PROPERTY = "measurement.histogram.verbose";
 
+  /**
+   * Whether or not to emit the histogram buckets.
+   */
+  private final boolean verbose;
+  
   private final List<Double> percentiles;
 
   public OneMeasurementHdrHistogram(String name, Properties props) {
     super(name);
     percentiles = getPercentileValues(props.getProperty(PERCENTILES_PROPERTY, PERCENTILES_PROPERTY_DEFAULT));
+    verbose = Boolean.valueOf(props.getProperty(VERBOSE_PROPERTY, String.valueOf(false)));
     boolean shouldLog = Boolean.parseBoolean(props.getProperty("hdrhistogram.fileoutput", "false"));
     if (!shouldLog) {
       log = null;
@@ -112,6 +124,21 @@ public class OneMeasurementHdrHistogram extends OneMeasurement {
     }
 
     exportStatusCounts(exporter);
+
+    // also export totalHistogram
+    if (verbose) {
+      for (HistogramIterationValue v : totalHistogram.recordedValues()) {
+        int value;
+        if (v.getValueIteratedTo() > (long)Integer.MAX_VALUE) {
+          value = Integer.MAX_VALUE;
+        } else {
+          value = (int)v.getValueIteratedTo();
+        }
+  
+        String opName = getName() + "-HISTOGRAM";
+        exporter.write(opName, Integer.toString(value), (double)v.getCountAtValueIteratedTo());
+      }
+    }
   }
 
   /**
